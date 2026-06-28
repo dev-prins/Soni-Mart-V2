cb2499967315e1082408abfeb55f5b80faf93663
Prins Soni <princesoni5010@gmail.com>
2026-06-28 23:31:41 +0530

    Refactor Firebase initialization and improve comments
    Removed unnecessary Firebase configuration fields and added a comment for clarity.

--- server.js
+++ server.js
@@ -6,17 +6,9 @@ const admin = require("firebase-admin");
 try {
   admin.initializeApp({
     credential: admin.credential.cert({
-      type: "service_account",
       project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID, // इसे एनवायरमेंट वेरिएबल से लें
       private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : '',
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-      universe_domain: "googleapis.com"
     })
   });
   console.log("Firebase Admin Initialized Successfully!");
@@ -32,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("Soni Mart Server is running fine!");
 });
 
-// इसे app.get कर दिया है ताकि ब्राउज़र में सीधे टेस्ट हो सके
+// प्रोडक्ट जोड़ने का रूट (GET इस्तेमाल किया है)
 app.get("/add-product", async (req, res) => {
   try {
     const newProduct = {
